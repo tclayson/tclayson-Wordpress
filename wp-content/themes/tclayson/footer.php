7385ea53b6c995fe3615d85a9120c6b3e86a157d
Thomas Clayson <tclayson@gmail.com>
2012-05-27 03:15:07 +0100

    Added tclayson theme and updated the README file.

---
+++ wp-content/themes/tclayson/footer.php
@@ -0,0 +1,13 @@
+		<div id="footer">
+			&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?> - Powered by <a href="http://pharma-mix.com">Pharma Mix</a>
+		</div>
+
+	</div>
+
+	<?php wp_footer(); ?>
+	
+	<!-- Don't forget analytics -->
+	
+</body>
+
+</html>
---
+++ wp-content/themes/tclayson/inc/meta.php
@@ -0,0 +1,7 @@
+<? if(!is_page()): ?>
+<div class="meta">
+	<em>Posted on:</em> <?php the_time('F jS, Y') ?>
+	<em>by</em> <?php the_author() ?>
+	<?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
+</div>
+<? endif; ?>
